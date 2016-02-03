6c18ee7e19f4172ea0c92a245381a1c295298d01
Omid Nasri <omid.nasri@outlook.com>
2016-02-03 20:13:06 +0330

    First Commit

---
+++ Configuring_Yahoo_Sign_In_for_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Configuring_Yahoo_Sign_In_for_MVC.MvcApplication" Language="C#" %>
---
+++ Configuring_Yahoo_Sign_In_for_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Configuring_Yahoo_Sign_In_for_MVC.Startup))]
+namespace Configuring_Yahoo_Sign_In_for_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
